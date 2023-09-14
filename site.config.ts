7472d445498259b139305464540149641d5f4662
0xJk <95065730+0xJk@users.noreply.github.com>
2023-09-14 22:31:51 +1000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,12 +17,12 @@ export default siteConfig({
   description: 'Chef Kang backyard',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '0xJam_',
+  //github: 'transitive-bullshit',
+  //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  newsletter: '#', // optional newsletter URL
+  youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -50,13 +50,13 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
+ // navigationStyle: 'default'
+   navigationStyle: 'custom',
+  navigationLinks: [
+     {
+       title: 'About',
+      pageId: '9df1ed52afe94b5f9dc4306441f996cb'
+     }
   //   {
   //     title: 'Contact',
   //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
