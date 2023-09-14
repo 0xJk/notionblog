f3ba0c9e649a78d3a6b02a7bef236f5a039b5880
0xJk <95065730+0xJk@users.noreply.github.com>
2023-09-14 21:51:25 +1000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '78731cd1f4d5444099d2798eabac1fbe',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Chef Kang康师傅的后花园',
+  domain: 'chefkang.xyz',
+  author: 'Chef Kang康师傅',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Chef Kang backyard',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
